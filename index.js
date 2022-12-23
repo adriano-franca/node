3abf728c9456efe5a79c073ad6afd8637efbbf9e
adriano0111 <adriano.franca@discente.ufma.br>
2022-12-23 09:52:15 -0300

    estudando query params node

--- index.js
+++ index.js
@@ -16,8 +16,13 @@ app.get("/blog/:artigo?", function(req, res){
     }
 });
 
-app.get("/canal/youtube", function(req, res){
-    res.send("www.youtube.com/");
+app.get("/canal/youtube/", function(req, res){
+    var canal = req.query["canal"];
+    if(canal){
+        res.send("www.tiktok.com/");
+    }else{
+        res.send("www.youtube.com/");
+    }
 });
 
 app.get("/ola/:nome", function(req, res){
